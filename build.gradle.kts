219050397aabb8be4a81ef89ef10f46f0f0001e9
Bernie Schelberg <bernard.schelberg@lens.org>
2021-04-28 11:48:30 +1000

    Another failing test
    Updated versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.4.21"
+    id("org.jetbrains.kotlin.jvm") version "1.5.0"
 }
 
 repositories {
@@ -13,9 +13,9 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 
-    testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.23")
+    testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.23.1")
 
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.1")
 }
 
 tasks.test {
--- src/test/kotlin/AssertkTest.kt
+++ src/test/kotlin/AssertkTest.kt
@@ -2,27 +2,21 @@ import assertk.all
 import assertk.assertThat
 import assertk.assertions.any
 import assertk.assertions.isEqualTo
-import assertk.assertions.prop
 import org.junit.jupiter.api.Test
 
 class AssertkTest {
 
     @Test
-    fun `should not pass`() {
-        val key1 = "key1"
-        val value1 = "value1"
+    fun `should not pass, pairs`() {
+        val key1 = "key 1"
+        val value1 = "value 1"
 
-        val key2 = "key2"
-        val value2 = "value2"
+        val pairs = listOf("key 2" to "value 2", "key 2" to "value 2", "key 2" to "value 2", key1 to value1)
 
-        val map: Map<String, Collection<Any>> = mapOf(key1 to listOf(value1), key2 to setOf(value2))
-
-        assertThat(map.entries).all {
-            any {
-                it.all {
-                    prop(Map.Entry<String, Any>::key).isEqualTo(key1) // assert 1
-                    prop(Map.Entry<Any, Collection<Any>>::key).isEqualTo(value1) // assert 2
-                }
+        assertThat(pairs).any {
+            it.all {
+                transform { it.first }.isEqualTo(key1)
+                transform { it.second }.isEqualTo("unexpected")
             }
         }
     }
